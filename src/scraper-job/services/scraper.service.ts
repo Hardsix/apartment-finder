12928c169f0c2c3aa7e842cd1c873a1ce1e37e2f
Tomislav Bradaric <seventh.advocate@gmail.com>
2021-09-11 18:59:47 +0200

    upgrade apify version

--- src/scraper-job/services/scraper.service.ts
+++ src/scraper-job/services/scraper.service.ts
@@ -1,171 +1,245 @@
-import * as bluebird from 'bluebird'
-import { launchPuppeteer } from 'apify'
-import * as _ from 'lodash'
-import { JSDOM } from 'jsdom'
-import { Apartment } from '~/apartment/entities/apartment.entity';
+import * as bluebird from "bluebird";
+import { launchPuppeteer } from "apify";
+import * as _ from "lodash";
+import { JSDOM } from "jsdom";
+import { Apartment } from "~/apartment/entities/apartment.entity";
 
-const stealthFetchSleepBaselineMs = 3000
+const stealthFetchSleepBaselineMs = 3000;
 
 const sleep = (ms) => {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms)
-  })
-}
+    setTimeout(resolve, ms);
+  });
+};
 
-let browser
+let browser;
 async function getBrowser() {
   if (browser) {
-    return browser
+    return browser;
   }
 
-  browser = await launchPuppeteer({ stealth: true });  
-  return browser
+  browser = await launchPuppeteer({
+    stealth: true,
+    launchOptions: {
+      executablePath: "/usr/bin/chromium-browser",
+    },
+  });
+  return browser;
 }
 
 async function fetchUrlInStealth(url: string): Promise<string> {
-  await sleep(_.floor((1 + Math.random()) * stealthFetchSleepBaselineMs))
+  await sleep(_.floor((1 + Math.random()) * stealthFetchSleepBaselineMs));
 
-  const browser = await getBrowser()
+  const browser = await getBrowser();
   const page = await browser.newPage();
-  
+
   await page.goto(url);
-  const content = await page.content()
+  const content = await page.content();
   await page.close();
 
-  return content
+  return content;
 }
 
 function isProcessed(pubDate: Date, lastProcessedDate: Date) {
-  return pubDate < lastProcessedDate
+  return pubDate < lastProcessedDate;
 }
 
-async function extractNjuskaloApartmentLinksFromPage(url: string, fetchAllPages = true, pageIndex = 1, processNewerThan: Date = undefined) {
-  const content = await fetchUrlInStealth(url)
+async function extractNjuskaloApartmentLinksFromPage(
+  url: string,
+  fetchAllPages = true,
+  pageIndex = 1,
+  processNewerThan: Date = undefined
+) {
+  const content = await fetchUrlInStealth(url);
 
   const dom = await new JSDOM(content);
 
-  const aptLinksProxy = dom.window.document.querySelectorAll('.content-main .EntityList-item .entity-body .entity-title .link')
-  let aptLinks = _.map(aptLinksProxy, (aptLink) => `https://njuskalo.hr${aptLink.href}`)
+  const aptLinksProxy = dom.window.document.querySelectorAll(
+    ".content-main .EntityList-item .entity-body .entity-title .link"
+  );
+  let aptLinks = _.map(
+    aptLinksProxy,
+    (aptLink) => `https://njuskalo.hr${aptLink.href}`
+  );
 
-  const pubDatesProxy = dom.window.document.querySelectorAll('.content-main .EntityList-item .entity-body .entity-pub-date .date--full')
-  let pubDates = _.map(pubDatesProxy, (pubDate) => new Date(pubDate.dateTime))
+  const pubDatesProxy = dom.window.document.querySelectorAll(
+    ".content-main .EntityList-item .entity-body .entity-pub-date .date--full"
+  );
+  let pubDates = _.map(pubDatesProxy, (pubDate) => new Date(pubDate.dateTime));
 
-  const apartmentLinkData = []
+  const apartmentLinkData = [];
   for (let i = 0; i < aptLinks.length; i++) {
     apartmentLinkData.push({
       link: aptLinks[i],
-      date: pubDates[i]
-    })
+      date: pubDates[i],
+    });
   }
 
   let apartmentLinksToProcess = _.map(
-    _.filter(apartmentLinkData, (data) => !processNewerThan || !isProcessed(data.date, processNewerThan)), (apt) => apt.link
-  )
+    _.filter(
+      apartmentLinkData,
+      (data) => !processNewerThan || !isProcessed(data.date, processNewerThan)
+    ),
+    (apt) => apt.link
+  );
 
   if (_.size(apartmentLinksToProcess) > 0 && fetchAllPages) {
-    const newPageUrl = `${url}&page=${pageIndex + 1}`
-    apartmentLinksToProcess = apartmentLinksToProcess.concat(await extractNjuskaloApartmentLinksFromPage(newPageUrl, true, pageIndex + 1, processNewerThan))
+    const newPageUrl = `${url}&page=${pageIndex + 1}`;
+    apartmentLinksToProcess = apartmentLinksToProcess.concat(
+      await extractNjuskaloApartmentLinksFromPage(
+        newPageUrl,
+        true,
+        pageIndex + 1,
+        processNewerThan
+      )
+    );
   }
 
-  return apartmentLinksToProcess
+  return apartmentLinksToProcess;
 }
 
-async function extractSingleApartmentDataFromNjuskaloPage(url: string): Promise<Partial<Apartment>> {
-  console.log(`Parsing apartment in url ${url}`)
+async function extractSingleApartmentDataFromNjuskaloPage(
+  url: string
+): Promise<Partial<Apartment>> {
+  console.log(`Parsing apartment in url ${url}`);
 
-  const content = await fetchUrlInStealth(url)
-  const data = extractSingleApartmentDataFromNjuskaloPageContent(content)
+  const content = await fetchUrlInStealth(url);
+  const data = extractSingleApartmentDataFromNjuskaloPageContent(content);
 
-  return data
+  return data;
 }
 
-async function extractSingleApartmentDataFromNjuskaloPageContent(content: string): Promise<Partial<Apartment>> {
+async function extractSingleApartmentDataFromNjuskaloPageContent(
+  content: string
+): Promise<Partial<Apartment>> {
   const dom = await new JSDOM(content);
-  const document = dom.window.document
+  const document = dom.window.document;
 
-  const name = document.querySelector('.ClassifiedDetailSummary-title').textContent
+  const name = document.querySelector(".ClassifiedDetailSummary-title")
+    .textContent;
 
-  const adCodeRaw = document.querySelector('.ClassifiedDetailSummary-adCode').textContent
-  const advertisementCode = _.trim(_.split(adCodeRaw, ':')[1])
+  const adCodeRaw = document.querySelector(".ClassifiedDetailSummary-adCode")
+    .textContent;
+  const advertisementCode = _.trim(_.split(adCodeRaw, ":")[1]);
 
-  const priceRaw = document.querySelector('.ClassifiedDetailSummary-priceForeign').textContent
-  const priceRegex = /([0-9.]+)/
-  const priceMatch = priceRegex.exec(priceRaw)
-  const priceEuros = priceMatch[0]
+  const priceRaw = document.querySelector(
+    ".ClassifiedDetailSummary-priceForeign"
+  ).textContent;
+  const priceRegex = /([0-9.]+)/;
+  const priceMatch = priceRegex.exec(priceRaw);
+  const priceEuros = priceMatch[0];
 
-  const basicDetailsRaw = document.querySelector('.ClassifiedDetailBasicDetails-list').children
-  const basicDetailsPairs: any[] = _.chunk(basicDetailsRaw, 2)
-  const basicDetails = {}
+  const basicDetailsRaw = document.querySelector(
+    ".ClassifiedDetailBasicDetails-list"
+  ).children;
+  const basicDetailsPairs: any[] = _.chunk(basicDetailsRaw, 2);
+  const basicDetails = {};
   _.forEach(basicDetailsPairs, (pair) => {
-    const [keyElem, valElem] = pair
-    basicDetails[_.trim(keyElem.textContent)] = _.trim(valElem.textContent)
-  })
+    const [keyElem, valElem] = pair;
+    basicDetails[_.trim(keyElem.textContent)] = _.trim(valElem.textContent);
+  });
 
-  const trimmedFloor = _.trim(basicDetails['Kat'], '.')
-  const floorRaw = trimmedFloor === 'Visoko prizemlje' ? '0' : trimmedFloor
+  const trimmedFloor = _.trim(basicDetails["Kat"], ".");
+  const floorRaw = trimmedFloor === "Visoko prizemlje" ? "0" : trimmedFloor;
 
   const result: Partial<Apartment> = {
     name,
     advertisementCode,
-    bedroomCount: _.toNumber(_.split(basicDetails['Broj soba'], '-')[0]),
-    city: _.split(basicDetails['Lokacija'], ',')[0],
-    neighbourhood: _.trim(_.split(basicDetails['Lokacija'], ',')[1]),
-    locationInNeighbourhood: _.trim(_.split(basicDetails['Lokacija'], ',')[2]),
+    bedroomCount: _.toNumber(_.split(basicDetails["Broj soba"], "-")[0]),
+    city: _.split(basicDetails["Lokacija"], ",")[0],
+    neighbourhood: _.trim(_.split(basicDetails["Lokacija"], ",")[1]),
+    locationInNeighbourhood: _.trim(_.split(basicDetails["Lokacija"], ",")[2]),
     floor: _.toNumber(floorRaw),
-    squareMeters: _.toNumber(_.replace(_.trim(basicDetails['Stambena površina'], ' m²'), ',', '.')),
-    priceEuros: _.toNumber(_.replace(priceEuros, '.', '')),
-    yearBuilt: _.toNumber(_.replace(basicDetails['Godina izgradnje'], '.', '')),
-    yearRenovated: _.toNumber(_.replace(basicDetails['Godina zadnje renovacije'], '.', '')),
-  }
+    squareMeters: _.toNumber(
+      _.replace(_.trim(basicDetails["Stambena površina"], " m²"), ",", ".")
+    ),
+    priceEuros: _.toNumber(_.replace(priceEuros, ".", "")),
+    yearBuilt: _.toNumber(_.replace(basicDetails["Godina izgradnje"], ".", "")),
+    yearRenovated: _.toNumber(
+      _.replace(basicDetails["Godina zadnje renovacije"], ".", "")
+    ),
+  };
 
   if (result.yearBuilt === 0) {
-    delete result.yearBuilt
+    delete result.yearBuilt;
   }
 
   if (_.isNaN(result.floor) || result.floor === null) {
-    delete result.floor
+    delete result.floor;
   }
 
   if (_.isNaN(result.bedroomCount) || result.bedroomCount === null) {
-    delete result.bedroomCount
+    delete result.bedroomCount;
   }
 
   if (result.yearRenovated === 0) {
-    delete result.yearRenovated
+    delete result.yearRenovated;
   }
 
-  return result
+  return result;
 }
 
-async function extractApartmentsDataFromNjuskaloPage(name, url: string, processNewerThan: Date) {
+async function extractApartmentsDataFromNjuskaloPage(
+  name,
+  url: string,
+  processNewerThan: Date
+) {
   try {
-    console.log(`[${name} - ${new Date()}] Processing scraper with options:\n${JSON.stringify({
+    console.log(
+      `[${name} - ${new Date()}] Processing scraper with options:\n${JSON.stringify(
+        {
+          url,
+          processNewerThan,
+        },
+        null,
+        2
+      )}`
+    );
+    const apartmentLinks = await extractNjuskaloApartmentLinksFromPage(
       url,
+      true,
+      1,
       processNewerThan
-    }, null, 2)}`)
-    const apartmentLinks = await extractNjuskaloApartmentLinksFromPage(url, true, 1, processNewerThan)
-    console.log(`[${name} - ${new Date()}] Located ${apartmentLinks.length} links to process`)
-  
-    const apartmentsData = await bluebird.mapSeries(apartmentLinks, async (link) => {
-      const data = await extractSingleApartmentDataFromNjuskaloPage(link)
-  
-      return {
-        link,
-        data,
-      }
-    })
-    console.log(`[${name} - ${new Date()}] Processed data for ${apartmentsData.length} apartments`)
+    );
+    console.log(
+      `[${name} - ${new Date()}] Located ${
+        apartmentLinks.length
+      } links to process`
+    );
 
-    return apartmentsData
+    const apartmentsData = await bluebird.mapSeries(
+      apartmentLinks,
+      async (link) => {
+        const data = await extractSingleApartmentDataFromNjuskaloPage(link);
+
+        return {
+          link,
+          data,
+        };
+      }
+    );
+    console.log(
+      `[${name} - ${new Date()}] Processed data for ${
+        apartmentsData.length
+      } apartments`
+    );
+
+    return apartmentsData;
   } catch (err) {
-    console.log(`[${name} - ${new Date()}] Encountered error:\n${JSON.stringify({
-      message: err.message,
-      stack: err.stack,
-      err,
-      name,
-      url,
-      processNewerThan,
-    }, null, 2)}`)
+    console.log(
+      `[${name} - ${new Date()}] Encountered error:\n${JSON.stringify(
+        {
+          message: err.message,
+          stack: err.stack,
+          err,
+          name,
+          url,
+          processNewerThan,
+        },
+        null,
+        2
+      )}`
+    );
   }
 }
 
@@ -173,4 +247,4 @@ export {
   extractSingleApartmentDataFromNjuskaloPageContent,
   extractNjuskaloApartmentLinksFromPage,
   extractApartmentsDataFromNjuskaloPage,
-}
+};
